9f6799cf7ce67aaa35d350ff6097ce56a74f09b2
MaryHanson11 <maryhanson@ufl.edu>
2022-10-30 16:57:41 -0400

    added functionality to profile
    User can add profile image and logout. Name and email display automatically. Profile image also displays if it was added already.

--- JobHunter/ProfileViewController.swift
+++ JobHunter/ProfileViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
@@ -18,13 +20,80 @@ class ProfileViewController: UIViewController {
     }
     
     @IBAction func logoutButton(_ sender: Any) {
-        //Add functionality
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        delegate.window?.rootViewController = loginViewController
+        
     }
+    
+    var addedProfileImage = false
+    
+    @IBAction func addProfileImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else{
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage // comes in a dictionary
+        
+        let size = CGSize(width: 500, height: 500)
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        profileImage.image = scaledImage
+        
+        let user = PFUser.current()!
+        
+        let imageData = profileImage.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        user["profileImage"] = file
+        
+        user.saveInBackground { (success, error) in
+            if success {
+                print("saved")
+                self.dismiss(animated: true, completion: nil)
+            } else{
+                print("error")
+            }
+        }
+        
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //sometimes this stuff just doesnt work
+        let user = PFUser.current()
+        
+        //make profile image view circular
+        profileImage.layer.cornerRadius = profileImage.frame.size.width/2
+        
+        nameLabel.text = user?.username
+        emailLabel.text = user?.email
+        
+        if (user?["profileImage"] != nil) {
+            let imageFile = user?["profileImage"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+        
+            profileImage.af.setImage(withURL: url)
+        }
+        
+        
+        
     }
     
 
